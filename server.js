7c9d935555ddd2d62a7e755da518e223f0571340
huliqun <huliquns@126.com>
2018-05-10 14:48:30 +0800

    init-project

---
+++ server.js
@@ -0,0 +1,98 @@
+// ExpressJS调用方式
+const express = require('express');
+const debug = require('debug')('demo:server');
+const http = require('http');
+
+let app = express();
+let port = 3000;
+
+
+// 引入NodeJS的子进程模块
+let child_process = require('child_process');
+
+app.get('/', function(req, res){
+
+    // 完整URL
+    // let url = req.protocol + '://'+ req.hostname + req.originalUrl;
+    let url='http://www.kbjbuy.com'
+
+    // 预渲染后的页面字符串容器
+    let content = '';
+
+    // 开启一个phantomjs子进程
+    let phantom = child_process.spawn('phantomjs', [__dirname + '/spider.js', url]);
+
+    // 设置stdout字符编码
+    phantom.stdout.setEncoding('utf8');
+
+    // 监听phantomjs的stdout，并拼接起来
+    phantom.stdout.on('data', function(data){
+        content += data.toString();
+    });
+
+    // 监听子进程退出事件
+    phantom.on('exit', function(code){
+        switch (code){
+            case 1:
+                console.log('加载失败');
+                res.send('加载失败');
+                break;
+            case 2:
+                console.log('加载超时: '+ url);
+                res.send(content);
+                break;
+            default:
+                res.send(content);
+                break;
+        }
+    });
+
+});
+
+app.set('port', port);
+let server = http.createServer(app);
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+
+    let bind = typeof port === 'string' ?
+        'Pipe ' + port :
+        'Port ' + port;
+
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+    console.log("services running on port " + port)
+    let addr = server.address();
+    let bind = typeof addr === 'string' ?
+        'pipe ' + addr :
+        'port ' + addr.port;
+    debug('Listening on ' + bind);
+}
